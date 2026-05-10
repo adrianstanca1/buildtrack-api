e5fb1fe861b4541f7ac33c797cc663e9f78aa8e1
BuildTrack CI <ci@cortexbuild.com>
2026-05-10 11:33:11 +0000

    chore: remove dist/ from git index (still built locally for PM2)

--- dist/routes/projects.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export { router as projectsRouter };
-//# sourceMappingURL=projects.d.ts.map
--- dist/routes/safety.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export { router as safetyRouter };
-//# sourceMappingURL=safety.d.ts.map
